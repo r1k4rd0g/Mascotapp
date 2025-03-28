66b9c80cbad1615a7058219d1f23369aa1782d2d
Ricardo Graña <ricardogb.812@gmail.com>
2025-03-28 12:23:01 -0300

    Fix index.js-router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,6 +2,10 @@ import { Router } from "express";
 import swaggerUiExpress from 'swagger-ui-express';
 import swaggerJsDoc from 'swagger-jsdoc';
 import {swaggerOptions} from '../docs/info.js';
+import countriesRouter from './countriesRouter.js';
+import statesRouter from './statesRouter.js';
+import citiesRouter from './citiesRouter.js';
+import neighborhoodsRouter from './neighborhoodsRouter.js';
 
 
 class MainRouter {
@@ -12,7 +16,11 @@ class MainRouter {
     initRoutes() {
         const specs = swaggerJsDoc(swaggerOptions);
         this.router.use('/docs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
-        //this.router.use()
+        this.router.use('/api/countries', countriesRouter);
+        this.router.use('/api/states', statesRouter);
+        this.router.use('/api/cities', citiesRouter);
+        this.router.use('/api/neighborhoods', neighborhoodsRouter);
+
     }
 
     getRouter(){
