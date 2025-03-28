82da2bec1651b45889faf92fa084f395c1c70b73
Ricardo Graña <ricardogb.812@gmail.com>
2025-03-28 14:10:56 -0300

    fix conflict factoryDao

--- server/src/persistence/daos/factoryDao.js
+++ server/src/persistence/daos/factoryDao.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 //Modules
 import config from "../../config/configEnv.js";
 import logger from "../../utils/logger/loggerWinston.js";
 import MongoConnection from "../../config/connection/mongoDBConnection.js";
 import MySQLConnection from "../../config/connection/mySqlDBConnection.js";
-=======
-/*** Modules  ****/
-import config from "../../config/configEnv";
-import logger from "../../utils/logger/loggerWinston";
-import MongoConnection from "../../config/connection/mongoDBConnection";
->>>>>>> 2a099d6139b39928a394870e1264031cdaa87479
 
-<<<<<<< HEAD
 /*** DAO de MongoDB  ****/
 import UserMongoDBDao from "./mongoDB/users/usersDao";
 import PetMongoDBDao from "./mongoDB/pets/petsDao";
@@ -19,15 +11,6 @@ import CountryMongoDBDao from "./mongoDB/countries/countriesDao";
 import StatesMongoDBDao from "./mongoDB/states/statesDao";
 import CitiesMongoDBDao from "./mongoDB/cities/citiesDao";
 import NeighborhoodMongoDBDao from "./mongoDB/neighborhoods/neighborhoodsDao";
-=======
-//DAO de MongoDB
-import UserMongoDBDao from "./mongoDB/users/usersDao.js";
-import PetMongoDBDao from "./mongoDB/pets/petsDao.js";
-import CountryMongoDBDao from "./mongoDB/locations/countries/countriesDao.js";
-import StatesMongoDBDao from "./mongoDB/locations/states/statesDao.js";
-import CitiesMongoDBDao from "./mongoDB/locations/cities/citiesDao.js";
-import NeighborhoodMongoDBDao from "./mongoDB/locations/neighborhoods/neighborhoodsDao.js";
->>>>>>> develop
 
 //DAO de MySQL
 //import UserMySQLDao from "./mySql/users/usersDao.js";
