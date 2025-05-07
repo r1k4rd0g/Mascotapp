4243c7aeaa3a491b7d1a6fff8906f0f940d8a9f8
Ricardo Graña <ricardogb.812@gmail.com>
2025-05-07 13:58:25 -0300

    hot fix in cors.js

--- server/src/config/security/cors.js
+++ server/src/config/security/cors.js
@@ -16,7 +16,7 @@ const corsOptions = {
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
-    allowedOrigins: 'Content-Type, Authorization',
+    allowedHeaders: 'Content-Type, Authorization',
 }
 
 export const corsConfig = cors(corsOptions);
